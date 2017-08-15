210f2f7ea6b2fa1590ad3077c19e2d5b25ec8c1b
venkatfriends <30984173+venkatfriends@users.noreply.github.com>
2017-08-15 23:17:21 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -51,7 +51,7 @@ submit.onclick = function(){
     }
     
     //Make the request
-   request.open('GET','http://venkatfriends.imad.hasura-app.io/counter', true);
+   request.open('GET','http://venkatfriends.imad.hasura-app.io/submit-name?name=', true);
    request.send(null); 
     
     
