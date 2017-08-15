b4b9fd0e3ac044a870986fb87704e2c6dd17f8d8
venkatfriends <30984173+venkatfriends@users.noreply.github.com>
2017-08-15 22:33:24 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -3,7 +3,24 @@ var button = document.getElementById('counter');
 var counter = 0;
 
 button.onclick = function(){
-    counter = counter + 1;
-    var span = document.getElementById('count');
-    span.innerHTML = counter.toString();
+    
+    //Make a request to the counter endpoint
+    var request = new XMLHttpRequest();
+    
+    //capture the response and store it in a variable
+    request.onreadystatechange = function(){
+        if(request.readyState === XMLHttpRequest.DONE){
+            //Take some action
+            if (request.status === 200){
+                var counter = request.responseText;
+                var span = document.getElementById('count');
+                span.innerHTML = counter.toString();
+            }
+        }
+        //Not done Yet
+    }
+    
+    //Make the request
+   request.open('GET','http://venkatfriends.imad.hasura-app.io/counter', true);
+   request.send(null);    
 };
