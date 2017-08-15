2014bc3232f779f267c1aeac7adfe714ec2b5518
venkatfriends <30984173+venkatfriends@users.noreply.github.com>
2017-08-15 23:13:57 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -28,7 +28,7 @@ app.get('/ui/madi.png', function (req, res) {
 });
 
 var names = [];
-app.get('/submit-name/:name', function(req, res){
+app.get('/submit-name', function(req, res){
    var name = req.query.name;
    names.push(name);
    
