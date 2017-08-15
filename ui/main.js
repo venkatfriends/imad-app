86791899975e3da8c588dc15dd4d1c7698a320fc
venkatfriends <30984173+venkatfriends@users.noreply.github.com>
2017-08-15 23:03:45 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -29,11 +29,30 @@ var nameInput = document.getElementById('name');
 var name= nameInput.value;
 var submit = document.getElementById('submit_btn');
 submit.onclick = function(){
-    var names = ['name1','name2','name3','name4'];
-    var list = '';
-    for (var i=0; i< names.length; i++){
-        list += '<li>' + names[i] + '</li>';
+    
+    var request = new XMLHttpRequest();
+    
+    //capture the response and store it in a variable
+    request.onreadystatechange = function(){
+        if(request.readyState === XMLHttpRequest.DONE){
+            //Take some action
+            if (request.status === 200){
+               var names = request.response.text;
+               names = JSON.parse(names);
+               var list = '';
+               for (var i=0; i< names.length; i++){
+                   list += '<li>' + names[i] + '</li>';
+               }
+               var ul = document.getElementById('namelist');
+               ul.innerHTML = list;
+            }
+        }
+        //Not done Yet
     }
-    var ul = document.getElementById('namelist');
-    ul.innerHTML = list;
+    
+    //Make the request
+   request.open('GET','http://venkatfriends.imad.hasura-app.io/counter', true);
+   request.send(null); 
+    
+    
 }
