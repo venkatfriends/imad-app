775709b1e60e2939769a6e8398e55ea79609e4f4
venkatfriends <30984173+venkatfriends@users.noreply.github.com>
2017-08-15 22:07:40 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,5 +1,7 @@
 //console.log('Loaded!');
 var button = document.getElementById('counter');
+var counter = 0;
+
 button.onclick = function(){
     counter = counter + 1;
     var span = document.getElementById('count');
